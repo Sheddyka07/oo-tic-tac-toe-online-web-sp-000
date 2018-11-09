d385d3c263c8971e1f7ddac34a754096b06d6e13
Kelly Sheddy <jungaka07@gmail.com>
2018-11-09 14:50:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -4,8 +4,8 @@ class TicTacToe
     @board = board
   end
 
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+def display_board
+  puts " #{@board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
