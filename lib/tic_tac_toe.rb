c0a7d6fccc3e4c991c7155fe3745dc0c924d5267
Kelly Sheddy <jungaka07@gmail.com>
2018-11-09 14:55:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,35 +29,19 @@ class TicTacToe
 
   def turn
     puts "Please enter 1-9:"
-    user_input = gets.strip
-  #input to index
-  index = input_to_index(user_input)
-  token = current_player(board)
-  
-  #check for validation
-  if valid_move?(board,index)
-    puts 'valid move'
-    move(board, index, token)
-    display_board(board)
-   else
-    puts 'try again'
-    turn(board)
-  end
-  display_board(board)
-end
-
-def turn
-    puts "Please enter 1-9:"
     input = gets.strip
     index = input.to_i - 1
-    if valid_move?(input)
-      player = current_player
-      move(input, player)
-      display_board
-    else
-      turn
-    end
+  if valid_move?(input)
+    player = current_player
+    move(input, player)
+    display_board
+  else
+    turn
   end
+end
+
+
+    
 
 
 
