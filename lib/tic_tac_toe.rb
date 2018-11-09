3bd345cf9d79499c089068467d95caf622d82f78
Kelly Sheddy <jungaka07@gmail.com>
2018-11-09 14:58:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,7 +3,10 @@ class TicTacToe
   def initialize(board = Array.new(9," "))
     @board = board
   end
-
+  
+  WIN_COMBINATIONS = [ 
+    [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
+  
   def display_board
     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
     puts "-----------"
@@ -57,30 +60,17 @@ def current_player
     return "O"
   end 
 end
-
-
-
-
-
-
-
   
-
-
-
-WIN_COMBINATIONS = [ 
-  [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
-  
-def won?(board)
+def won?
   WIN_COMBINATIONS.detect do |win_combination|
   
   win_index_1 = win_combination[0]
   win_index_2 = win_combination[1]
   win_index_3 = win_combination[2]
   
-  position_1 = board[win_index_1]
-  position_2 = board[win_index_2]
-  position_3 = board[win_index_3]
+  position_1 = @board[win_index_1]
+  position_2 = @board[win_index_2]
+  position_3 = @board[win_index_3]
   
   if position_1 == "X" && position_2 == "X" && position_3 == "X"
     return win_combination 
@@ -90,7 +80,16 @@ def won?(board)
     false 
   end
 end
-end
+
+
+
+
+
+
+
+
+
+
 
 def full?(board)
   if board.detect{|i| i == " " || i == nil}
