fdf1fe606ca9a9ecd4baa58bd119bca34d201823
Kelly Sheddy <jungaka07@gmail.com>
2018-11-09 14:53:10 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -22,6 +22,9 @@ class TicTacToe
     @board[index] != " " && @board[index] != ""
   end
 
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
 
 
 
@@ -36,9 +39,6 @@ end
 
 
 
-def valid_move?(board, index)
-  index.between?(0,8) && !position_taken?(board, index)
-end
 
 def turn(board)
   puts "Please enter 1-9:"
